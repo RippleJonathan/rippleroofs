ce7452ed3870a508e4b98cb3ab6382384d6a0f64
Jonathan <jonathan@rippleroofs.com>
2025-11-14 09:38:34 -0600

    Fix Nextdoor URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -23,7 +23,7 @@ export const SITE_CONFIG = {
     youtube: '', // Coming soon
     tiktok: '', // Coming soon
     googleBusiness: 'https://share.google/SsBLQ1RNm8ZQ7rFrk',
-    nextdoor: 'https://nextdoor.com/pages/ripple-roofing-construction?utm_campaign=1763133725160&share_action_id=e429fede-3025-4ae2-aff7-761adab1bbc7',
+    nextdoor: 'https://nextdoor.com/page/ripple-roofing-construction/',
   },
 }
 
