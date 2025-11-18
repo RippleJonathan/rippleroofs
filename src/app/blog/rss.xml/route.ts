07f7f3cdb87d81ad4519d8ef21212f828f987b40
Jonathan <jonathan@rippleroofs.com>
2025-11-18 08:56:49 -0600

    Fix RSS feed - wrap all text content in CDATA sections

--- src/app/blog/rss.xml/route.ts
+++ src/app/blog/rss.xml/route.ts
@@ -1,19 +1,12 @@
 import { getAllPosts } from '@/lib/blog'
 import { SITE_CONFIG } from '@/lib/constants'
 
-// Helper function to escape XML special characters
-function escapeXml(text: string): string {
-  return text
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&apos;')
-}
-
 export async function GET() {
   const posts = getAllPosts()
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
+  
+  // Use a safe title without special characters for XML elements
+  const safeTitle = 'Ripple Roofing and Construction - Roofing Blog'
 
   const rssXml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" 
@@ -21,15 +14,15 @@ export async function GET() {
      xmlns:dc="http://purl.org/dc/elements/1.1/"
      xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>${escapeXml(SITE_CONFIG.name)} - Roofing Blog</title>
+    <title><![CDATA[${safeTitle}]]></title>
     <link>${siteUrl}/blog</link>
-    <description>Expert roofing tips, maintenance guides, and industry insights from Central Texas roofing professionals</description>
+    <description><![CDATA[Expert roofing tips, maintenance guides, and industry insights from Central Texas roofing professionals]]></description>
     <language>en-us</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
     <atom:link href="${siteUrl}/blog/rss.xml" rel="self" type="application/rss+xml"/>
     <image>
       <url>${siteUrl}/logo.png</url>
-      <title>${escapeXml(SITE_CONFIG.name)}</title>
+      <title><![CDATA[${safeTitle}]]></title>
       <link>${siteUrl}</link>
     </image>
     ${posts
@@ -53,7 +46,7 @@ export async function GET() {
 
   return new Response(rssXml, {
     headers: {
-      'Content-Type': 'application/xml',
+      'Content-Type': 'application/xml; charset=utf-8',
       'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate',
     },
   })
